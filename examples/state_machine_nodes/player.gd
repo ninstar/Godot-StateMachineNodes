c19352e208a5b75645145b9306a329c15f04693b
ninstar <ninstar@pm.me>
2024-06-18 01:12:37 -0300

    Set InputMap at runtime
    This is just to uncouple the example scene from the project file.

--- examples/state_machine_nodes/player.gd
+++ examples/state_machine_nodes/player.gd
@@ -7,6 +7,7 @@ var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _ready() -> void:
+	load_input_map()
 	$State.text = state_machine.initial_state.name
 
 
@@ -22,3 +23,36 @@ func _physics_process(delta: float) -> void:
 
 func _on_state_machine_state_changed(_previous_state: String, new_state: String) -> void:
 	$State.text = new_state
+
+
+func load_input_map() -> void:
+	var add_keys = func(action: StringName, keycodes: Array) -> void:
+		if not InputMap.has_action(action):
+			InputMap.add_action(action)
+		for keycode in keycodes:
+			var event := InputEventKey.new()
+			event.physical_keycode = keycode
+			InputMap.action_add_event(action, event)
+	var add_pads = func(action: StringName, button_indexes: Array) -> void:
+		if not InputMap.has_action(action):
+			InputMap.add_action(action)
+		for button_index in button_indexes:
+			var event := InputEventJoypadButton.new()
+			event.button_index = button_index
+			InputMap.action_add_event(action, event)
+		
+	# Jump
+	add_keys.call(&"jump", [KEY_SPACE, KEY_UP, KEY_W])
+	add_pads.call(&"jump", [JOY_BUTTON_DPAD_UP, JOY_BUTTON_A, JOY_BUTTON_B])
+	
+	# Crouch
+	add_keys.call(&"crouch", [KEY_DOWN, KEY_S])
+	add_pads.call(&"crouch", [JOY_BUTTON_DPAD_DOWN])
+	
+	# Walk (Left)
+	add_keys.call(&"walk_left", [KEY_LEFT, KEY_A])
+	add_pads.call(&"walk_left", [JOY_BUTTON_DPAD_LEFT])
+
+	# Walk (Right)
+	add_keys.call(&"walk_right", [KEY_RIGHT, KEY_D])
+	add_pads.call(&"walk_right", [JOY_BUTTON_DPAD_RIGHT])
